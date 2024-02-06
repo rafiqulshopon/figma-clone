872c80d847ed9c851f27656537eeb6bfb7fb627c
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-02-06 16:11:43 +0600

    update assets file

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1 className='font-5xl text-white'>Hello</h1>;
+  return (
+    <div className='h-[100vh] w-full flex justify-center items-center text-center'>
+      <h1 className='text-2xl text-white'>Hello</h1>
+    </div>
+  );
 }
