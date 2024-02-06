590ba04806aefba6e986e105ae5edf901d379024
Rafiqul Shopon <rafiqul.dev@gmail.com>
2024-02-06 17:15:03 +0600

    trying to fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Live from '@/components/Live';
+
 export default function Page() {
   return (
     <div className='h-[100vh] w-full flex justify-center items-center text-center'>
       <h1 className='text-2xl text-white'>Hello</h1>
+      <Live />
     </div>
   );
 }
